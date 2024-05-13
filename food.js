25fb9d58fc1ffb42223686bf0d0bca1211b34df1
Jaden <jadenglenn@pursuit.org>
2024-05-13 15:59:57 -0400

    Part 2 complete

--- food.js
+++ food.js
@@ -20,6 +20,37 @@ class Food {
     }
 }
 
+class BadFood extends Food {
+    constructor (name, daysToSpoil = 20, fresh = true) {
+        super (name, daysToSpoil, fresh);
+         this.weapons = [
+            {name : "Mouth Burn",  hitPoints : 3},
+            {name : "Sugar Shock", hitPoints : 4},
+            {name : "Hot Pepper Storm",
+            hitPoints : 5
+            }
+        ];
+    }
+    prepare() {
+        console.log(`I am ${this.name} and my calories are too high to count!`)
+    }
+    isFresh() {
+        if (this.daysToSpoil <= 0) {
+            this.fresh = false
+        } else {
+            console.log("The food is still in the fight")
+        }
+    }
+    fight(badFood) {
+        const weaponPicker = Math.floor(Math.random() * 3);
+         let weapon = this.weapons[weaponPicker]
+
+          badFood.daysToSpoil -= weapon.hitPoints
+             
+           console.log(`${badFood.name} is only has ${badFood.daysToSpoil} days before being spoiled ,
+            but I am still at ${this.daysToSpoil} days!`)
+    }
+}
 
 
 
