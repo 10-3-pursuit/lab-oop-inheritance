bbb8412575380960100e8728d212581e8db28192
Jaden <jadenglenn@pursuit.org>
2024-05-13 13:17:55 -0400

    part 1 complete

--- food.js
+++ food.js
@@ -1,4 +1,27 @@
-// Create class below
+class Food {
+    constructor (name, daysToSpoil, fresh = true) {
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = fresh;
+    }
+    Prepare() {
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh() {
+        if (this.daysToSpoil > 0) {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} goes bad`)
+        } else {
+            console.log(`The expiration date has passed and ${this.name} is no longer edible`)
+        }
+    }
+    aDayPasses() {
+        this.daysToSpoil --
+         this.isFresh()
+    }
+}
 
-// Do not edit below this line
+
+
+
+
 module.exports = Food
