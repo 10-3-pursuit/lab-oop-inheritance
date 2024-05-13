81e53a01f84ea731addb2d9a8a2919be05f3a55b
juligarc91 <julissagarcia@pursuit.org>
2024-05-13 15:29:56 -0400

    created food class and added attributes and prepare method

--- food.js
+++ food.js
@@ -1,4 +1,13 @@
 // Create class below
-
+class food {
+    constructor(name, daysToSpoil, fresh){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.fresh = !fresh ? fresh : true;
+    }
+    prepare(food) {
+        console.log(`${food.name} is being prepared`)
+    }
+}
 // Do not edit below this line
 module.exports = Food
