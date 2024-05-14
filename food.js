cfdfc2e59d5e8bcdab9aeb003e0386adaa1005a0
Iris Beckham <irisdancer3@gmail.com>
2024-05-14 15:25:17 -0700

    add isFresh method to check how long the food is good for

--- food.js
+++ food.js
@@ -8,6 +8,13 @@ class Food {
     prepare() {
         console.log(this.name)
     }
+    isFresh() {
+        if (this.daysToSpoil <= 0) {
+            console.log(`${this.name} has spoiled.`)
+        } else {
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+        }
+    }
 }
 // Do not edit below this line
 module.exports = Food
