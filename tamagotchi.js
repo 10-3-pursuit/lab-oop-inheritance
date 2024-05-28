9ff0aec6f22d870b8739c72cb47c88d03521dfd2
Iris Beckham <irisdancer3@gmail.com>
2024-05-28 16:06:45 -0700

    status method

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,14 +1,21 @@
 // Create class below
 class Tamagotchi {
-    constructor(name, energy = 9, full = 6, sick = false, rehomed = false) {
+    constructor(name, energy = 9, full = 6, mood = 6, sick = false, rehomed = false) {
         this.name = name;
         this.energy = energy;
         this.full = full;
+        this.mood = mood;
         this.sick = sick;
         this.rehomed = rehomed;
     }
     greet() {
         console.log(`Hello, I'm ${this.name}!`)
+    }
+    status() {
+        console.log(
+            `My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}\n`,
+            this.sick ? `I am sick` : `I am not sick`
+        )
     }
 }
 
