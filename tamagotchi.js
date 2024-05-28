5f2ee79312c69b3452524a64059f57b47d5720e9
Iris Beckham <irisdancer3@gmail.com>
2024-05-28 15:59:17 -0700

    tamagotchi constructor

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,13 @@
 // Create class below
+class Tamagotchi {
+    constructor(name, energy = 9, full = 6, sick = false, rehomed = false) {
+        this.name = name;
+        this.energy = energy;
+        this.full = full;
+        this.sick = sick;
+        this.rehomed = rehomed;
+    }
+}
 
 // Do not edit below this line
 module.exports = Tamagotchi
