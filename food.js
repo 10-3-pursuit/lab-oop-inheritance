750e2e7c34ba6a1fc6f08ccb64de8bd7cf1aaf68
Iris Beckham <irisdancer3@gmail.com>
2024-05-28 20:34:57 -0700

    fight method

--- food.js
+++ food.js
@@ -22,19 +22,56 @@ class Food {
 }
 
 class BadFood extends Food {
-    constructor(name, daysToSpoil = 20, fresh = true, weapons = [{ hp: 3 }, { hp: 4 }, { hp: 5 }]) {
-        this.name = name;
-        this.daysToSpoil = daysToSpoil;
-        this.fresh = fresh;
+    constructor(name, daysToSpoil = 20, fresh = true, weapons = [{ hitPoints: 3 }, { hitPoints: 4 }, { hitPoints: 5 }]) {
+        super(name, daysToSpoil, fresh)
         this.weapons = weapons;
     }
-    preapare() {
+    prepare() {
         console.log(`I am ${this.name} and my calories are too high to count!`)
     }
     fight(opponent) {
-        opponent.daysToSpoil -= this.weapons[(Math.floor(Math.random() * (2 + 1)))].hp;
-        console.log(`${opponent.name} only has ${opponent.daysToSpoil} days before being spoiled , but I am still at ${this.daysToSpoil} days!`)
+        opponent.daysToSpoil -= this.weapons[(Math.floor(Math.random() * (2 + 1)))].hitPoints;
+        console.log(`${opponent.name} only has ${opponent.daysToSpoil} days before being spoiled, but I am still at ${this.daysToSpoil} days!`)
     }
 }
+const pizzaWeapons = [
+    {
+        name: 'Mouth Burn',
+        hitPoints: 3,
+    },
+    {
+        name: 'Hot Pepper Storm',
+        hitPoints: 4,
+    },
+    {
+        name: 'Cheese Grease',
+        hitPoints: 5,
+    },
+]
+const donutWeapons = [
+    {
+        name: 'Sprinkle Spray',
+        hitPoints: 3,
+    },
+    {
+        name: 'Icing Deluge',
+        hitPoints: 4,
+    },
+    {
+        name: 'Sugar Shock',
+        hitPoints: 5,
+    },
+]
+const pizza = new BadFood("Pizza", 20, true, pizzaWeapons);
+const donut = new BadFood("Donut", 20, true, donutWeapons);
+
+pizza.prepare();
+donut.prepare();
+pizza.fight(donut);
+donut.fight(pizza);
+pizza.fight(donut);
+donut.fight(pizza);
+
+
 // Do not edit below this line
 module.exports = Food
